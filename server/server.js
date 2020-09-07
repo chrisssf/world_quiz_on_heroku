619ea9e27055b769531c6f124fd521083e740a48
Chris Fraser <chrisssfraser@gmail.com>
2020-09-08 00:13:59 +0100

    more fixes

--- server/server.js
+++ server/server.js
@@ -26,7 +26,7 @@ const createRouter = require('./helpers/create_router.js');
 // .catch( console.error );
 
 // new code!!!!!!!!!!
-MongoClient.connect('mongodb+srv://Chrisssf:Chrimssf@freecluster.zfuvg.mongodb.net/world_quiz?retryWrites=true&w=majority' , {useUnifiedTopology: true})
+MongoClient.connect( process.env.DATABASE_URL , {useUnifiedTopology: true})
   .then((client) => {
     const db = client.db('world_quiz');
     const countriesCollection = db.collection('countries');
