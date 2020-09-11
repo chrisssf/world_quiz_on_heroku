2e82d91329de08e5e3966484de1359b20dde3c07
Chris Fraser <chrisssfraser@gmail.com>
2020-09-11 01:55:17 +0100

    added more easy questions for each quiz

--- server/db/seed_data.js
+++ server/db/seed_data.js
@@ -50,7 +50,58 @@ db.countries.insertMany([
     "Question": "Which country on the map is Saudi Arabia?",
     "Options": [[{v: "Algeria", f: "?"}, {v:0, f:""}], [{v: "Saudi Arabia", f: "?"}, {v:1, f:""}], [{v: "Sudan", f: "?"}, {v:2, f:""}], [{v: "Kazakhstan", f: "?"}, {v:3, f:""}]],
     "Answer": "Saudi Arabia"
-  }
+  },
+  {
+    "Question": "Which country on the map is Australia?",
+    "Options": [[{v: "Australia", f: "?"}, {v:0, f:""}], [{v: "Japan", f: "?"}, {v:1, f:""}], [{v: "India", f: "?"}, {v:2, f:""}], [{v: "China", f: "?"}, {v:3, f:""}]],
+    "Answer": "Australia"
+  },
+  {
+    "Question": "Which country on the map is Greenland?",
+    "Options": [[{v: "Greenland", f: "?"}, {v:0, f:""}], [{v: "Sweden", f: "?"}, {v:1, f:""}], [{v: "New Zealand", f: "?"}, {v:2, f:""}], [{v: "Iceland", f: "?"}, {v:3, f:""}]],
+    "Answer": "Greenland"
+  },
+  {
+    "Question": "Which country on the map is Iceland?",
+    "Options": [[{v: "France", f: "?"}, {v:0, f:""}], [{v: "Madagascar", f: "?"}, {v:1, f:""}], [{v: "Canada", f: "?"}, {v:2, f:""}], [{v: "Iceland", f: "?"}, {v:3, f:""}]],
+    "Answer": "Iceland"
+  },
+  {
+    "Question": "Which country on the map is Great Britain?",
+    "Options": [[{v: "RU", f: "?"}, {v:0, f:""}], [{v: "France", f: "?"}, {v:1, f:""}], [{v: "GB", f: "?"}, {v:2, f:""}], [{v: "United States", f: "?"}, {v:3, f:""}]],
+    "Answer": "GB"
+  },
+  {
+    "Question": "Which country on the map is Spain?",
+    "Options": [[{v: "Germany", f: "?"}, {v:0, f:""}], [{v: "Algeria", f: "?"}, {v:1, f:""}], [{v: "Brazil", f: "?"}, {v:2, f:""}], [{v: "Spain", f: "?"}, {v:3, f:""}]],
+    "Answer": "Spain"
+  },
+  {
+    "Question": "Which country on the map is Argentina?",
+    "Options": [[{v: "Argentina", f: "?"}, {v:0, f:""}], [{v: "Peru", f: "?"}, {v:1, f:""}], [{v: "Mexico", f: "?"}, {v:2, f:""}], [{v: "Angola", f: "?"}, {v:3, f:""}]],
+    "Answer": "Argentina"
+  },
+  {
+    "Question": "Which country on the map is Afghanistan?",
+    "Options": [[{v: "Sweden", f: "?"}, {v:0, f:""}], [{v: "Afghanistan", f: "?"}, {v:1, f:""}], [{v: "Botswana", f: "?"}, {v:2, f:""}], [{v: "Mauritania", f: "?"}, {v:3, f:""}]],
+    "Answer": "Afghanistan"
+  },
+  {
+    "Question": "Which country on the map is United States?",
+    "Options": [[{v: "Canada", f: "?"}, {v:0, f:""}], [{v: "Australia", f: "?"}, {v:1, f:""}], [{v: "United States", f: "?"}, {v:2, f:""}], [{v: "RU", f: "?"}, {v:3, f:""}]],
+    "Answer": "United States"
+  },
+  {
+    "Question": "Which country on the map is Madagascar?",
+    "Options": [[{v: "Madagascar", f: "?"}, {v:0, f:""}], [{v: "Kenya", f: "?"}, {v:1, f:""}], [{v: "Namibia", f: "?"}, {v:2, f:""}], [{v: "Somalia", f: "?"}, {v:3, f:""}]],
+    "Answer": "Madagascar"
+  },
+  {
+    "Question": "Which country on the map is New Zealand?",
+    "Options": [[{v: "Japan", f: "?"}, {v:0, f:""}], [{v: "New Zealand", f: "?"}, {v:1, f:""}], [{v: "Philippines", f: "?"}, {v:2, f:""}], [{v: "Indonesia", f: "?"}, {v:3, f:""}]],
+    "Answer": "New Zealand"
+  },
+  
 
 ]);
 db.capitals.insertMany([
@@ -207,4 +258,60 @@ db.flags.insertMany([
     "Question": "https://restcountries.eu/data/bra.svg",
     "Options": [[{v: "Mexico", f: "Mexico"}, {v:0, f:""}], [{v: "Canada", f: "Canada"}, {v:1, f:""}], [{v: "Colombia", f: "Colombia"}, {v:2, f:""}], [{v: "Brazil", f: "Brazil"}, {v:3, f:""}]],
     "Answer": "Brazil"
-  }]);
+  },
+  {
+    "Question": "https://restcountries.eu/data/usa.svg",
+    "Options": [[{v: "United States", f: "United States"}, {v:0, f:""}], [{v: "New Zealand", f: "New Zealand"}, {v:1, f:""}], [{v: "Brazil", f: "Brazil"}, {v:2, f:""}], [{v: "Russia", f: "Russia"}, {v:3, f:""}]],
+    "Answer": "United States"
+  },
+  {
+    "Question": "https://restcountries.eu/data/gbr.svg",
+    "Options": [[{v: "Spain", f: "Spain"}, {v:0, f:""}], [{v: "GB", f: "Great Britain"}, {v:1, f:""}], [{v: "Australia", f: "Australia"}, {v:2, f:""}], [{v: "Norway", f: "Norway"}, {v:3, f:""}]],
+    "Answer": "GB"
+  },
+  {
+    "Question": "https://restcountries.eu/data/can.svg",
+    "Options": [[{v: "Canada", f: "Canada"}, {v:0, f:""}], [{v: "Russia", f: "Russia"}, {v:1, f:""}], [{v: "Greenland", f: "Greenland"}, {v:2, f:""}], [{v: "Algeria", f: "Algeria"}, {v:3, f:""}]],
+    "Answer": "Canada"
+  },
+  {
+    "Question": "https://restcountries.eu/data/deu.svg",
+    "Options": [[{v: "Spain", f: "Spain"}, {v:0, f:""}], [{v: "Bolivia", f: "Bolivia"}, {v:1, f:""}], [{v: "Ethiopia", f: "Ethiopia"}, {v:2, f:""}], [{v: "Germany", f: "Germany"}, {v:3, f:""}]],
+    "Answer": "Germany"
+  },
+  {
+    "Question": "https://restcountries.eu/data/nor.svg",
+    "Options": [[{v: "Kazakhstan", f: "Kazakhstan"}, {v:0, f:""}], [{v: "Norway", f: "Norway"}, {v:1, f:""}], [{v: "Venezuela", f: "Venezuela"}, {v:2, f:""}], [{v: "Indonesia", f: "Indonesia"}, {v:3, f:""}]],
+    "Answer": "Norway"
+  },
+  {
+    "Question": "https://restcountries.eu/data/fin.svg",
+    "Options": [[{v: "Nigeria", f: "Nigeria"}, {v:0, f:""}], [{v: "Germany", f: "Germany"}, {v:1, f:""}], [{v: "Mexico", f: "Mexico"}, {v:2, f:""}], [{v: "Finland", f: "Finland"}, {v:3, f:""}]],
+    "Answer": "Finland"
+  },
+  {
+    "Question": "https://restcountries.eu/data/mex.svg",
+    "Options": [[{v: "Libya", f: "Libya"}, {v:0, f:""}], [{v: "Iceland", f: "Iceland"}, {v:1, f:""}], [{v: "Sweden", f: "Sweden"}, {v:2, f:""}], [{v: "Mexico", f: "Mexico"}, {v:3, f:""}]],
+    "Answer": "Mexico"
+  },
+  {
+    "Question": "https://restcountries.eu/data/chl.svg",
+    "Options": [[{v: "Colombia", f: "Colombia"}, {v:0, f:""}], [{v: "Chile", f: "Chile"}, {v:1, f:""}], [{v: "Kenya", f: "Kenya"}, {v:2, f:""}], [{v: "Pakistan", f: "Pakistan"}, {v:3, f:""}]],
+    "Answer": "Chile"
+  },
+  {
+    "Question": "https://restcountries.eu/data/ury.svg",
+    "Options": [[{v: "Belarus", f: "Belarus"}, {v:0, f:""}], [{v: "Mongolia", f: "Mongolia"}, {v:1, f:""}], [{v: "Uruguay", f: "Uruguay"}, {v:2, f:""}], [{v: "Greenland", f: "Greenland"}, {v:3, f:""}]],
+    "Answer": "Uruguay"
+  },
+  {
+    "Question": "https://restcountries.eu/data/rus.svg",
+    "Options": [[{v: "Ukraine", f: "Ukraine"}, {v:0, f:""}], [{v: "Peru", f: "Peru"}, {v:1, f:""}], [{v: "Russia", f: "Russia"}, {v:2, f:""}], [{v: "Iceland", f: "Iceland"}, {v:3, f:""}]],
+    "Answer": "Russia"
+  },
+  {
+    "Question": "https://restcountries.eu/data/tur.svg",
+    "Options": [[{v: "Chad", f: "Chad"}, {v:0, f:""}], [{v: "France", f: "France"}, {v:1, f:""}], [{v: "Colombia", f: "Colombia"}, {v:2, f:""}], [{v: "Turkey", f: "Turkey"}, {v:3, f:""}]],
+    "Answer": "Turkey"
+  }
+]);
